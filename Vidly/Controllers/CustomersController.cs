1327c23e830dbc2d4d13d44e9126498c2fc15d58
Sergii <sergeyshovkunenko@gmail.com>
2019-09-17 20:58:32 -0700

    Customer Edit function is implemented

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -24,14 +24,23 @@ namespace Vidly.Controllers
         public ActionResult New()
         {
             var membershipTypes = _context.MembershipTypes.ToList();
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
                 Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
-            return View("New", viewModel);
+            return View("CustomerForm", viewModel);
         }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+                
+            return RedirectToAction("Index","Customers");
+        }  
 
         public ViewResult Index()
         {
@@ -48,6 +57,22 @@ namespace Vidly.Controllers
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.FirstOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
         
     }
